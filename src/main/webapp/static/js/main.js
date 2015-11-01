e7c8a8ff54a54e64f35e3169df4d82ff88c4567e
Sergii Roshchupkin <sergey.roshchupkin@gmail.com>
2015-11-01 23:08:32 +0100

    add static files

---
+++ src/main/webapp/static/js/main.js
@@ -0,0 +1,5 @@
+function validateURL(textval) {
+    var urlregex = new RegExp(
+        "^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.){1}([0-9A-Za-z]+\.)");
+    return urlregex.test(textval);
+}
